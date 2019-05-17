a7455b49d6bcd70aa73cdd0d4e7379a83d9b4212
Manes Munyanyi <44741475+mmunyanyi@users.noreply.github.com>
2019-05-17 08:11:50 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,8 +1,11 @@
 
 sourceDataFile <- "C:/DataScience/ExDataAssignment1/household_power_consumption.txt"
 data <- read.table(sourceDataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#Triming data to only required dataset
 trimData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-#str(trimData)
+
+#Convert text to numeric
 globalActivePower <- as.numeric(trimData$Global_active_power)
 png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
